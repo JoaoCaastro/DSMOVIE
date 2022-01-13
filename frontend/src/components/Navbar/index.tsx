690e5bb4bfda93447ee5a9b2dd2c24afb759dff8
Joao Castro <cjoao.contato@gmail.com>
2022-01-13 20:52:44 -0300

    Form layout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,22 +1,22 @@
-import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
 
 function Navbar() {
 
-    return(
+    return (
         <header>
-        <nav className="container">
-          <div className="dsmovie-nav-content">
-            <h1>DSMovie</h1>
-            <a href="https://github.com/JoaoCaastro" target="_blank" rel="noreferrer">
-              <div className="dsmovie-contact-container">
-                <GithubIcon />
-                <p className="dsmovie-contact-link">/JoaoCastro</p>
-              </div>
-            </a>
-          </div>
-        </nav>
-      </header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/JoaoCaastro" target="_blank" rel="noreferrer">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/JoaoCastro</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
     );
 }
 
